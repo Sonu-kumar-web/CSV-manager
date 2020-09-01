8c55e2578aa22f5b72bbb61f67cd2416c347d93b
Sonu <sonu3660@gmail.com>
2020-09-02 00:03:13 +0530

    Added upload function

--- controllers/csvController.js
+++ controllers/csvController.js
@@ -1,63 +1,61 @@
-const FileSchema=require('../models/csvFile');
-const parse=require("csv-parser");
-const fs = require('fs');
+const FileSchema = require("../models/csvFile");
+const parse = require("csv-parser");
+const fs = require("fs");
 
-module.exports.ViewUploadForm=function(req,res){
-  res.render('upload', {
-    title: "CSV Manager | Upload CSV",
-  });
-}
+module.exports.ViewUploadForm = function (req, res) {
+   res.render("upload", {
+      title: "CSV Manager | Upload CSV",
+   });
+};
 
-module.exports.upload=function(req, res, next){
-  const file = req.file;
-  if (!file) {
-      const error = new Error('Please upload a file');
+module.exports.upload = function (req, res, next) {
+   const file = req.file;
+   if (!file) {
+      const error = new Error("Please upload a file");
       error.httpStatusCode = 400;
       return next(error);
-  }
-  FileSchema.create(file, function(err, file){
-      if(err){
-          console.log(err);
+   }
+   FileSchema.create(file, function (err, file) {
+      if (err) {
+         console.log(err);
       }
       console.log(file);
-  });
-  res.send(file);
-  
-}
+   });
+   res.send(file);
+};
 
-module.exports.getList=function(req, res){
-  const list=FileSchema.find({}, function(err, csv_list){
-    if(err){
-      console.log('error');
-      return;
-    }
-    res.render('uploadList', {
-      title: "CSV Manager | Upload List",
-      csv_list: csv_list,
-    });
-  });
-  
-}
+module.exports.getList = function (req, res) {
+   const list = FileSchema.find({}, function (err, csv_list) {
+      if (err) {
+         console.log("error");
+         return;
+      }
+      res.render("uploadList", {
+         title: "CSV Manager | Upload List",
+         csv_list: csv_list,
+      });
+   });
+};
 
-module.exports.csvDisplay=function(req,res){
-  FileSchema.findById(req.params.id, function(err, file){
-    if(err){
-      console.log(err);
-      return;
-    }
-    let file_path=file.path;
-    const data = []
-    fs.createReadStream(file_path)
-      .pipe(parse({ delimiter: ',' }))
-      .on('data', (r) => {
-        // console.log(r);
-        data.push(r);        
-      }) 
-      .on('end', () => {
-        res.render('csvDisplay', {
-          title: "CSV Manager | Display CSV",
-          data: data,
-        });
-      })
-  });
-}
+module.exports.csvDisplay = function (req, res) {
+   FileSchema.findById(req.params.id, function (err, file) {
+      if (err) {
+         console.log(err);
+         return;
+      }
+      let file_path = file.path;
+      const data = [];
+      fs.createReadStream(file_path)
+         .pipe(parse({ delimiter: "," }))
+         .on("data", (r) => {
+            // console.log(r);
+            data.push(r);
+         })
+         .on("end", () => {
+            res.render("csvDisplay", {
+               title: "CSV Manager | Display CSV",
+               data: data,
+            });
+         });
+   });
+};
