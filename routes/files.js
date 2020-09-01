6a0b59d5ab6c9e37b26ad5c27d190ccc2c054682
Sonu <sonu3660@gmail.com>
2020-09-01 23:58:23 +0530

    Add multer

--- routes/files.js
+++ routes/files.js
@@ -1,40 +1,46 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const multer=require('multer');
+const multer = require("multer");
 // var upload = multer({ dest: 'uploads/' });
-const csvController = require('../controllers/csvController');
-
+const csvController = require("../controllers/csvController");
 
 //defining multer storage as disk storage for local storage
-var storage=multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null,'uploads/');
-    },
-    filename: function(req, file,cb){
-        cb(null, file.fieldname+"-"+Date.now()+"."+ file.originalname.split('.')[file.originalname.split('.').length -1]);
-    },
+var storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, "uploads/");
+   },
+   filename: function (req, file, cb) {
+      cb(
+         null,
+         file.fieldname +
+            "-" +
+            Date.now() +
+            "." +
+            file.originalname.split(".")[
+               file.originalname.split(".").length - 1
+            ]
+      );
+   },
 });
 
 //defining upload specifications
-var upload=multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-        if (!file.originalname.match(/\.(csv)$/)) {
-            // return cb(null, false)
-            return cb(new Error('Only csv files are allowed!'));
-        }
-        cb(null, true);
-    }
+var upload = multer({
+   storage: storage,
+   fileFilter: function (req, file, cb) {
+      if (!file.originalname.match(/\.(csv)$/)) {
+         // return cb(null, false)
+         return cb(new Error("Only csv files are allowed!"));
+      }
+      cb(null, true);
+   },
 });
 
-
-
 //different routes
-router.get("/",csvController.getList);
-router.get("/new",csvController.ViewUploadForm);
+router.get("/", csvController.getList);
+router.get("/new", csvController.ViewUploadForm);
 
-router.post("/new", upload.single('csvFile') , csvController.upload);
+router.post("/new", upload.single("csvFile"), csvController.upload);
 
 router.get("/:id", csvController.csvDisplay);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-const homeController=require('../controllers/homeController');
+const homeController = require("../controllers/homeController");
 
-
-router.get('/', homeController.home);
-router.use('/files',require('./files'));
-module.exports=router;
+router.get("/", homeController.home);
+router.use("/files", require("./files"));
+module.exports = router;
